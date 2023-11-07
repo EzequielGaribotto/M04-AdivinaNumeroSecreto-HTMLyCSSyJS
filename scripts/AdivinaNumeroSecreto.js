132e3b37434ede9416b3b0b9f623c8faa7d102b4
EzequielGaribotto <ezequiel.garibotto.7e7@itb.cat>
2023-11-07 19:38:58 +0100

    funciones para sonidos y class

--- scripts/AdivinaNumeroSecreto.js
+++ scripts/AdivinaNumeroSecreto.js
@@ -1,16 +1,16 @@
 const numeroAleatorio = Math.floor(10000 + Math.random() * 90000);
 console.log(numeroAleatorio)
 var digitosCorrectos = 0
-var iteraciones = 0
-var maxIteracion = 5
+var intentos = 5
+const maxIteracion = 5
 var victoria = false
+let info = document.getElementById('user_message')
 function comprobarInput() {
     let info = document.getElementById('user_message')
     // Leemos el input del usuario
     let input = document.getElementsByTagName("input");
     let numero = input[0].value;
-    let inputCorrecto = false;
-    if (maxIteracion > iteraciones && !victoria) {
+    if (intentos != 0 && !victoria) {
         // Si el numero es un dígito positivo de como máximo 5 dígitos
         if (numero > -1 && numero < 100000) {
             // Contamos sus digitos
@@ -18,48 +18,43 @@ function comprobarInput() {
             for (digito in numero) {
                 digitosCounter++
             }
-            // Si los digitos son exactamente 5
+            // Si los digitos son exactamente 5 insertamos una fila
             if (digitosCounter == 5) {
-                // Cambiamos el mensaje de información para el usuario 
-                info.innerHTML = "Input correcto... Analizando resultado"
-                // Indicamos que el input es correcto
-                inputCorrecto = true;
-                // Si el input resulta ser correcto insertamos una fila
                 insertarFila(numero)
             } else {
                 // Si no tiene 5 digitos se lo decimos al usuario
-                info.innerHTML = "El numero debe tener 5 dígitos, solo tiene "+digitosCounter
-                document.getElementById('invalid_input_sound').play();
+                info.innerHTML = "El numero debe tener 5 dígitos, tiene "+digitosCounter
+                playSound("bonk")
             }
         } else {
             // Si no es correcto, le decimos que ponga un numero de 5 dígitos
             info.innerHTML = "Input incorrecto, introduce un numero positivo de 5 dígitos"
-            document.getElementById('invalid_input_sound').play();
+            playSound("bonk")
         }
-    } else if (maxIteracion <= iteraciones) {
+    } else if (intentos == 0) {
         info.innerHTML = "Has consumido todos tus intentos. Reinicia la pagina para volver a jugar";
-        document.getElementById('invalid_input_sound').play();
+        playSound("bonk")
     } else {
         info.innerHTML = "Ya has ganado, reinicia la pagina para volver a jugar";
     }
     input[0].value = ""
 }
-
-function insertarFila(num) {
+function insertarFila(fila) {
     let info = document.getElementById('user_message')
-    iteraciones++
-    console.log(iteraciones)
+    intentos--
+    console.log(intentos)
     var filas = document.getElementById("filas_container");
     var nuevaFila = document.createElement("div");
-    var digitosCorrectos = 0
+    var digitosCorrectos = 0;
+    
     for (let digito = 0; digito < 5; digito++) {
         var nuevoDigito = document.createElement("div");
-        nuevoDigito.innerHTML = num[digito];
+        nuevoDigito.innerHTML = fila[digito];
         nuevaFila.appendChild(nuevoDigito);
         var greenSet = false
         var yellowSet = false
         for (let i = 0; i < 5; i++) {
-            if (num[digito] == numeroAleatorio.toString()[i]) {
+            if (fila[digito] == numeroAleatorio.toString()[i]) {
                 if (digito == i) {
                     digitosCorrectos++
                     // Color verde
@@ -75,30 +70,49 @@ function insertarFila(num) {
                 nuevoDigito.style.backgroundColor = "#989898";
             }
         }
+        nuevoDigito.classList.add("numero");
     }
     if (digitosCorrectos == 5) {
-        info.innerHTML = "Has acertado, enhorabuena!";
-        let numerosTop = document.getElementById('numeros_container') 
-        document.getElementById('numero_1').innerHTML = num[0]
-        document.getElementById('numero_2').innerHTML = num[1]
-        document.getElementById('numero_3').innerHTML = num[2]
-        document.getElementById('numero_4').innerHTML = num[3]
-        document.getElementById('numero_5').innerHTML = num[4]
-        numerosTop.style.paddingTop = 0;
-        numerosTop.style.alignSelf = "center";
-        document.getElementById('victory_sound').play()
-        victoria = true
-        document.getElementById('comprobar').style.backgroundColor = "gray";
-
+        finJuego("victoria")
     } else {
-        let intentosRestantes = maxIteracion-iteraciones
-        if (intentosRestantes == 0) {
-            info.innerHTML = "No ha habido suerte :("
-            document.getElementById('no_luck_sound').play();
+        if (intentos == 0) {
+            finJuego("derrota")
         } else {
-            info.innerHTML = "Has acertado "+ digitosCorrectos + " digito(s). Te quedan " + intentosRestantes + " intentos";
-            document.getElementById('miss_sound').play();
+            playSound("miss")
+            info.innerHTML = "Has acertado "+ digitosCorrectos + " digito(s). Te quedan " + intentos + " intentos";
         }
     }
-    filas.insertBefore(nuevaFila, filas.firstChild)
+    filas.appendChild(nuevaFila)
+    nuevaFila.classList.add("fila");
+}
+
+
+function playSound(sound) {
+    if (sound == "victoria") {
+        document.getElementById('victory_sound').play();
+    } else if (sound == "derrota") {
+        document.getElementById('no_luck_sound').play();
+    } else if (sound == "miss") {
+        document.getElementById('miss_sound').play();
+    } else if (sound == "bonk") {
+        document.getElementById('invalid_input_sound').play();
+    }
+}
+
+function finJuego(estado) {
+    if (estado == "victoria") {
+        document.getElementById('user_message').innerHTML = "Has acertado, enhorabuena!";
+        victoria = true
+    } else if (estado == "derrota") {
+        document.getElementById('user_message').innerHTML = "No ha habido suerte :("
+    }
+    mostrarNumeroSecreto()
+    playSound(estado)
+    document.getElementById('comprobar').style.backgroundColor = "gray";
+}
+
+function mostrarNumeroSecreto() {
+    for (let i = 0; i < 5; i++) {
+        document.getElementsByClassName('numeros')[i].innerHTML = numeroAleatorio.toString()[i]
+    }
 }
